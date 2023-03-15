3348f4d3d3df0c4940ba8c68b5740161e8ae527c
root <admin@example.com>
2023-03-15 14:12:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> YoYo testing suman:-)
+          Edit <code>src/App.js</code> YoYo testing suman first react project:-)
         </p>
         <a
           className="App-link"
